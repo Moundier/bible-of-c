179e16a5aad3f51040013fedc5786c603d82019d
Moundier <garamoundier.developer@protonmail.com>
2023-12-12 21:40:05 -0300

    refactor: ident tab-space size 2!

--- src/try.c
+++ src/try.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 struct Exception {
-    int type;
+  int type;
 };
 
 typedef void (*ExceptionHandler)(struct Exception*);
 
 void handle_exception(struct Exception* exception) {
-    printf("Exception caught: Type %d\n", exception->type);
+  printf("Exception caught: Type %d\n", exception->type);
 }
 
 struct Exception* exception_info = NULL;
@@ -18,46 +18,46 @@ ExceptionHandler exception_handler = handle_exception;
 typedef int (*ExceptionCheck)();
 
 int check_exception() {
-    return (exception_info != NULL) ? 1 : 0;
+  return (exception_info != NULL) ? 1 : 0;
 }
 
 #define TRY \
-    struct Exception exception; \
-    if (check_exception() == 0) \
+  struct Exception exception; \
+  if (check_exception() == 0) \
 
 #define CATCH(exception_type) \
-    else if (exception_info->type == exception_type)
+  else if (exception_info->type == exception_type)
 
 #define END_TRY \
-    free(exception_info); \
-    exception_info = NULL;
+  free(exception_info); \
+  exception_info = NULL;
 
 #define THROW(exception_type) \
-    do { \
-        if (!(exception_info = (struct Exception*)malloc(sizeof(struct Exception)))) { \
-            fprintf(stderr, "Failed to allocate memory for exception_info\n"); \
-            exit(1); \
-        } \
-        exception_info->type = exception_type; \
-        if (exception_handler != NULL) { \
-            exception_handler(exception_info); \
-        } \
-    } while (0)
+  do { \
+    if (!(exception_info = (struct Exception*)malloc(sizeof(struct Exception)))) { \
+      fprintf(stderr, "Failed to allocate memory for exception_info\n"); \
+      exit(1); \
+    } \
+    exception_info->type = exception_type; \
+    if (exception_handler != NULL) { \
+      exception_handler(exception_info); \
+    } \
+  } while (0)
 
 int main() {
-    TRY {
-        // Code that may throw an exception
-        int divisor = 0;
-        if (divisor == 0) {
-            THROW(1); // 1 represents a divide by zero exception
-        }
-        int result = 10 / divisor;
-        printf("Result: %d\n", result);
+  TRY {
+    // Code that may throw an exception
+    int divisor = 0;
+    if (divisor == 0) {
+      THROW(1); // 1 represents a divide by zero exception
     }
-    CATCH(1) {
-        printf("Caught divide by zero exception\n");
-    }
-    END_TRY;
+    int result = 10 / divisor;
+    printf("Result: %d\n", result);
+  }
+  CATCH(1) {
+    printf("Caught divide by zero exception\n");
+  }
+  END_TRY;
 
-    return 0;
+  return 0;
 }
